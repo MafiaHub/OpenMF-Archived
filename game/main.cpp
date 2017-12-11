53cd9343d8ff2aa7dbb0899efb2a730be025bfa1
Dominik Madarász <zaklaus@outlook.com>
2017-12-12 00:45:30 +0100

    Added vs2017 project.

--- game/main.cpp
+++ game/main.cpp
@@ -8,6 +8,7 @@ int main( int argc, char** argv )
 {
     std::cout << "test" << std::endl;
     osgViewer::Viewer viewer;
+	viewer.setUpViewInWindow(0, 0, 800, 600);
     osg::ref_ptr<osg::ShapeDrawable> s = new osg::ShapeDrawable(new osg::Sphere(osg::Vec3f(0,0,0),1));
     viewer.setSceneData(s);
     return viewer.run();
