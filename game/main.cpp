e89987be447c43dd7e049ab06c9177b4734eace1
Miloslav Číž <tastyfish@seznam.com>
2017-12-13 16:49:01 +0100

    Create resources folder and texture a quad

--- game/main.cpp
+++ game/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fstream>
+#include <osg/Node>
 #include <osgDB/ReadFile>
 #include <osgViewer/Viewer>
 #include <osg_loader.hpp>
+#include <osgDB/ReadFile>
+#include <osg/Texture2D>
 
 int main( int argc, char** argv )
 {
@@ -11,6 +14,13 @@ int main( int argc, char** argv )
     osgViewer::Viewer viewer;
     std::ifstream f;
     viewer.setUpViewInWindow(30, 30, 800, 600);
-    viewer.setSceneData( loader.load4ds(f) );
+
+    osg::ref_ptr<osg::Node> n = loader.load4ds(f);
+    osg::ref_ptr<osg::Texture2D> t = new osg::Texture2D();
+    osg::ref_ptr<osg::Image> i = osgDB::readImageFile("../resources/test.tga");
+    t->setImage(i);
+    n->getOrCreateStateSet()->setTextureAttributeAndModes(0,t.get());
+    
+    viewer.setSceneData(n);
     return viewer.run();
 }
