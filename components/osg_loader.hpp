f006e1ae57a001535096eec88c6b769b7bcf5645
Miloslav Číž <tastyfish@seznam.com>
2017-12-13 15:50:48 +0100

    Make geometry in loader indexed

--- components/osg_loader.hpp
+++ components/osg_loader.hpp
@@ -16,20 +16,46 @@ osg::ref_ptr<osg::Node> Loader::load4ds(std::ifstream &srcFile)
 {
     osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
     osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array;
+    osg::ref_ptr<osg::Vec2Array> uvs = new osg::Vec2Array;
 
-    normals->push_back(osg::Vec3(0.0f,-1.0f, 0.0f));
+    osg::ref_ptr<osg::DrawElementsUInt> indices = new osg::DrawElementsUInt(GL_TRIANGLES,6);
+
+    osg::Vec3 defaultNormal = osg::Vec3(-1,-1,-1);
+
+    // create vertices:
 
     vertices->push_back( osg::Vec3f(0,0,0) );
+    normals->push_back( defaultNormal );
+    uvs->push_back( osg::Vec2f(0,0) );
+
     vertices->push_back( osg::Vec3f(0,0,1) );
+    normals->push_back( defaultNormal );
+    uvs->push_back( osg::Vec2f(0,1) );
+
     vertices->push_back( osg::Vec3f(1,0,0) );
+    normals->push_back( defaultNormal );
+    uvs->push_back( osg::Vec2f(1,0) );
+
+    vertices->push_back( osg::Vec3f(1,0,1) );
+    normals->push_back( defaultNormal );
+    uvs->push_back( osg::Vec2f(1,1) );
+
+    // indices for indexed drawing:
+
+    (*indices)[0] = 0;
+    (*indices)[1] = 1;
+    (*indices)[2] = 2;
+    (*indices)[3] = 1;
+    (*indices)[4] = 2;
+    (*indices)[5] = 3;
 
     osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
 
     geom->setVertexArray(vertices.get());
     geom->setNormalArray(normals.get());
-    geom->setNormalBinding(osg::Geometry::BIND_OVERALL);
+    geom->setTexCoordArray(0,uvs.get());
 
-    geom->addPrimitiveSet(new osg::DrawArrays(GL_TRIANGLES,0,vertices->size()));
+    geom->addPrimitiveSet(indices.get());
 
     osg::ref_ptr<osg::Geode> geode = new osg::Geode;
     geode->addDrawable(geom.get());
