a445a4d0be9d60f3ab81273bb4a65d9037921bfb
Miloslav Číž <tastyfish@seznam.com>
2017-12-13 21:55:01 +0100

    Start DTA loader + enable c++14

--- components/format_parsers.hpp
+++ components/format_parsers.hpp
@@ -1,4 +1,6 @@
 #include <fstream>
+#include <iostream>
+#include <cstdint>
 
 namespace formats
 {
@@ -8,8 +10,8 @@ namespace formats
 class DataFormat
 {
 public:
-    virtual bool load(std::ifstream srcFile)=0;
-    virtual bool save(std::ofstream dstFile)=0;
+    virtual bool load(std::ifstream &srcFile)=0;
+    virtual bool save(std::ofstream &dstFile)=0;
 };
 
 class DataFormat4DS: public DataFormat
@@ -19,5 +21,28 @@ class DataFormat4DS: public DataFormat
 class DataFormat5DS: public DataFormat
 {
 };
+
+class DataFormatDTA: public DataFormat
+{
+public:
+    virtual bool load(std::ifstream &srcFile) override;
+    virtual bool save(std::ofstream &dstFile) override;
+};
+
+bool DataFormatDTA::load(std::ifstream &srcFile)
+{
+    int32_t header;
+    srcFile.read((char *) &header,4);
+
+    if (header != 0x30445349)
+      return false;
+
+    return true;
+}
+
+bool DataFormatDTA::save(std::ofstream &srcFile)
+{
+    return true;
+}
 
 }
