193583d502a4ac97581cb29ac75695431484b5cd
Miloslav Číž <tastyfish@seznam.com>
2017-12-13 14:40:07 +0100

    Add basic code for geometry creation to loader

--- components/osg_loader.hpp
+++ components/osg_loader.hpp
@@ -1,6 +1,6 @@
 #include <osg/Node>
-#include <osg/Shape>
-#include <osg/ShapeDrawable>
+#include <osg/Geometry>
+#include <osg/Geode>
 #include <fstream>
 
 namespace MFFormat
@@ -14,8 +14,27 @@ public:
 
 osg::ref_ptr<osg::Node> Loader::load4ds(std::ifstream &srcFile)
 {
-    osg::ref_ptr<osg::ShapeDrawable> result = new osg::ShapeDrawable(new osg::Sphere(osg::Vec3f(0,0,0),1));
-    return result;
+    osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
+    osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array;
+
+    normals->push_back(osg::Vec3(0.0f,-1.0f, 0.0f));
+
+    vertices->push_back( osg::Vec3f(0,0,0) );
+    vertices->push_back( osg::Vec3f(0,0,1) );
+    vertices->push_back( osg::Vec3f(1,0,0) );
+
+    osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
+
+    geom->setVertexArray(vertices.get());
+    geom->setNormalArray(normals.get());
+    geom->setNormalBinding(osg::Geometry::BIND_OVERALL);
+
+    geom->addPrimitiveSet(new osg::DrawArrays(GL_TRIANGLES,0,vertices->size()));
+
+    osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+    geode->addDrawable(geom.get());
+
+    return geode;
 }
 
 }
