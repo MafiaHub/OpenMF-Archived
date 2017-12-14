af83bd9fc59cc287571c53d85a2d94d52c00f4d2
Miloslav Číž <tastyfish@seznam.com>
2017-12-14 21:33:45 +0100

    Add more code to DTA loader

--- components/format_parsers.hpp
+++ components/format_parsers.hpp
@@ -1,8 +1,9 @@
-#include <fstream>
+
 #include <iostream>
 #include <cstdint>
+#include <vector>
 
-namespace formats
+namespace MFformat
 {
 
 /// Abstract class representing a game data format.
@@ -11,7 +12,10 @@ class DataFormat
 {
 public:
     virtual bool load(std::ifstream &srcFile)=0;
-    virtual bool save(std::ofstream &dstFile)=0;
+    virtual bool save(std::ofstream &dstFile) { return false; /* optional */ };
+
+protected:
+    std::streamsize fileLength(std::ifstream &f);
 };
 
 class DataFormat4DS: public DataFormat
@@ -42,17 +46,91 @@ public:
     {
         uint32_t mUnknown;
         uint32_t mDataOffset;
-        uint32_t mUnknown2;
-        char name[16];
+        uint32_t mDataEnd;
+        char mName[16];
     } ContentHeader;
+
+    typedef struct
+    {
+        uint32_t mUnknown;
+        uint32_t mUnknown2;
+        uint32_t mUnknown3;
+        uint32_t mUnknown4;
+        uint32_t mSize;
+        uint32_t mUnknown6;
+        unsigned char mNameLength;
+        unsigned char mUnknown7[7];
+        unsigned char mName[256];
+    } DataHeader;
 
 protected:
     void decrypt(char *buffer, unsigned int bufferLen, uint32_t key1, uint32_t key2);
 
-    FileHeader mHeader;
+    FileHeader mFileHeader;
+    std::vector<ContentHeader> mContentHeaders;
     uint32_t mKey1;
     uint32_t mKey2;
 };
+
+std::streamsize DataFormat::fileLength(std::ifstream &f)
+{
+    std::streampos fsize = f.tellg();
+    f.seekg( 0, std::ios::end );
+    fsize = f.tellg() - fsize;
+    f.seekg( 0, std::ios_base::beg );
+    return fsize;
+}
+
+bool DataFormatDTA::load(std::ifstream &srcFile)
+{
+    int32_t magicNo;
+
+    srcFile.read((char *) &magicNo,4);
+
+    if (magicNo != 0x30445349)     // magic number == "ISD0"?
+        return false;
+
+    // read the file header:
+
+    srcFile.read((char *) &mFileHeader,sizeof(FileHeader));
+    decrypt((char *) &mFileHeader,sizeof(mFileHeader),mKey1,mKey2);
+
+    // read the content headers:
+        
+    srcFile.seekg(mFileHeader.mContentOffset);
+    mContentHeaders.clear();
+
+    unsigned int headerArraySize = mFileHeader.mFileCount * sizeof(ContentHeader);
+    ContentHeader *headerArray = (ContentHeader *) malloc(headerArraySize);
+    srcFile.read((char *) headerArray,headerArraySize);
+    decrypt((char *) headerArray,headerArraySize,mKey1,mKey2);
+
+    for (int i = 0; i < mFileHeader.mFileCount; ++i)
+        mContentHeaders.push_back(headerArray[i]);
+
+    free(headerArray);
+
+    // read each file:
+
+    for (int i = 0; i < mContentHeaders.size(); ++i)
+    {
+        char s[17];
+        memcpy(s,mContentHeaders[i].mName,16);
+        s[16] = 0;
+
+        DataHeader h;
+
+        srcFile.seekg(mContentHeaders[i].mDataOffset);
+        srcFile.read((char *) &h,sizeof(DataHeader));
+        decrypt((char *) &h,sizeof(DataHeader),mKey1,mKey2);
+
+        h.mName[h.mNameLength] = 0;
+
+        std::cout << mContentHeaders[i].mName << ": " << h.mName << std::endl;
+    }
+
+    return true;
+}
 
 void DataFormatDTA::setDecryptKeys(uint32_t key1, uint32_t key2)
 {
@@ -77,29 +155,9 @@ void DataFormatDTA::decrypt(char *buffer, unsigned int bufferLen, uint32_t key1,
     }
 }
 
-bool DataFormatDTA::load(std::ifstream &srcFile)
-{
-    int32_t magicNo;
-    srcFile.read((char *) &magicNo,4);
-
-    if (magicNo != 0x30445349)     // "ISD0"
-      return false;
-
-    srcFile.read((char *) &mHeader,sizeof(FileHeader));
-
-    decrypt((char *) &mHeader,sizeof(mHeader),0xa94b8d3c,0x771f3888);
-
-    std::cout << mHeader.mFileCount << std::endl;
-    std::cout << mHeader.mContentOffset << std::endl;
-    std::cout << mHeader.mContentSize << std::endl;
-    std::cout << mHeader.mUnknown << std::endl;
-
-    return true;
-}
-
 bool DataFormatDTA::save(std::ofstream &srcFile)
 {
-    return true;
+    return false;
 }
 
 }
