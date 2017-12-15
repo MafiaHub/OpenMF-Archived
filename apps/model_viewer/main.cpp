b2e8aab0b887b8f1795701bc1a148e68256a0124
Miloslav Číž <tastyfish@seznam.com>
2017-12-15 17:32:12 +0100

    Correct formatting

--- apps/model_viewer/main.cpp
+++ apps/model_viewer/main.cpp
@@ -12,51 +12,18 @@ int main( int argc, char** argv )
 {
     MFFormat::Loader loader;
 
-    std::ifstream f;
-
-    MFformat::DataFormatDTA dta;
-    std::ifstream dtaFile("../mafia/A5.dta",std::ios::in|std::ios::binary);
-
-dta.setDecryptKeys(MFformat::DataFormatDTA::A5_KEYS);
-dta.load(dtaFile);
-
-std::cout << dta.getNumFiles() << std::endl;
-std::cout << dta.getFileName(0) << std::endl;
-std::cout << dta.getFileName(1) << std::endl;
-std::cout << dta.getFileName(2) << std::endl;
-
-/*
-std::ofstream outF;
-outF.open("out.wav",std::ios::out | std::ios::binary);
-
-
-char *b;
-unsigned int l;
-dta.getFile(dtaFile,0,&b,l);
-
-for (int i = 0; i < l; ++i)
-    outF << ((char) b[i]);
-
-//outF.write(b,l);
-
-outF.flush();
-outF.close();
-free(b);
-*/
-
-    dtaFile.close();
-
-return 0;
-
     osgViewer::Viewer viewer;
-    viewer.setUpViewInWindow(30, 30, 800, 600);
+
+    std::ifstream f;
+    f.open("../mafia/MODELS/baliksena.4ds");
 
     osg::ref_ptr<osg::Node> n = loader.load4ds(f);
     osg::ref_ptr<osg::Texture2D> t = new osg::Texture2D();
     osg::ref_ptr<osg::Image> i = osgDB::readImageFile("../resources/test.tga");
     t->setImage(i);
+
+    f.close();
     n->getOrCreateStateSet()->setTextureAttributeAndModes(0,t.get());   
     viewer.setSceneData(n);
     return viewer.run();
-
 }
--- components/osg_loader.hpp
+++ components/osg_loader.hpp
@@ -15,44 +15,44 @@ public:
 
 osg::ref_ptr<osg::Node> Loader::load4ds(std::ifstream &srcFile)
 {
-	MFformat::DataFormat4DS format;
-	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+    MFformat::DataFormat4DS format;
+    osg::ref_ptr<osg::Geode> geode = new osg::Geode;
 
-	if (format.load(srcFile))
-	{
-		auto model = format.getModel();
-		auto lod = model->mMeshes[0].mStandard.mLODs[0];
+    if (format.load(srcFile))
+    {
+        auto model = format.getModel();
+        auto lod = model->mMeshes[0].mStandard.mLODs[0];
 
-		osg::ref_ptr<osg::Vec3Array> vertices	= new osg::Vec3Array;
-		osg::ref_ptr<osg::Vec3Array> normals	= new osg::Vec3Array;
-		osg::ref_ptr<osg::Vec2Array> uvs		= new osg::Vec2Array;
+        osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
+        osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array;
+        osg::ref_ptr<osg::Vec2Array> uvs = new osg::Vec2Array;
 
-		for (size_t i = 0; i < lod.mVertexCount; ++i)
-		{
-			auto vertex = lod.mVertices[i];
+        for (size_t i = 0; i < lod.mVertexCount; ++i)
+        {
+            auto vertex = lod.mVertices[i];
 
-			vertices->push_back(osg::Vec3f(vertex.mPos.x, vertex.mPos.y, vertex.mPos.z));
-			normals->push_back(osg::Vec3f(vertex.mNormal.x, vertex.mNormal.y, vertex.mNormal.z));
-			uvs->push_back(osg::Vec2f(vertex.mUV.x, vertex.mUV.y));
-		}
+            vertices->push_back(osg::Vec3f(vertex.mPos.x, vertex.mPos.y, vertex.mPos.z));
+            normals->push_back(osg::Vec3f(vertex.mNormal.x, vertex.mNormal.y, vertex.mNormal.z));
+            uvs->push_back(osg::Vec2f(vertex.mUV.x, vertex.mUV.y));
+        }
 
-		osg::ref_ptr<osg::DrawElementsUInt> indices = new osg::DrawElementsUInt(GL_TRIANGLES, 6);
-		for (size_t i = 0; i < lod.mFaceGroups[0].mFaceCount; ++i)
-		{
-			auto face = lod.mFaceGroups[0].mFaces[i];
-			indices->push_back(face.mA);
-			indices->push_back(face.mB);
-			indices->push_back(face.mC);
-		}
+        osg::ref_ptr<osg::DrawElementsUInt> indices = new osg::DrawElementsUInt(GL_TRIANGLES, 6);
+        for (size_t i = 0; i < lod.mFaceGroups[0].mFaceCount; ++i)
+        {
+            auto face = lod.mFaceGroups[0].mFaces[i];
+            indices->push_back(face.mA);
+            indices->push_back(face.mB);
+            indices->push_back(face.mC);
+        }
 
-		osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
-		geom->setVertexArray(vertices.get());
-		geom->setNormalArray(normals.get());
-		geom->setTexCoordArray(0, uvs.get());
+        osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
+        geom->setVertexArray(vertices.get());
+        geom->setNormalArray(normals.get());
+        geom->setTexCoordArray(0, uvs.get());
 
-		geom->addPrimitiveSet(indices.get());
-		geode->addDrawable(geom.get());
-	}
+        geom->addPrimitiveSet(indices.get());
+        geode->addDrawable(geom.get());
+    }
 
     return geode;
 }
