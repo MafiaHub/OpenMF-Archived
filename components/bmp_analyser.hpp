0121ff56088c7d5187771a0f1f2d34ed3b6fa219
Miloslav Číž <tastyfish@seznam.com>
2017-12-26 13:54:14 +0100

    Remove tab

--- components/bmp_analyser.hpp
+++ components/bmp_analyser.hpp
@@ -27,7 +27,7 @@ bool BMPInfo::load(std::ifstream &f)
     char buffer[64];
     f.read(buffer,54);                      // headers after which there is a colortable
     f.read((char *) &mTransparentColor,4);  // the first color in the table is transparent
-	return true;
+    return true;
 }
 
 }
