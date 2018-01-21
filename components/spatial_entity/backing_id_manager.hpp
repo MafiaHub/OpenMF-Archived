1313943a5a792e437adc7933c860eba4972801f1
Dominik Madarász <zaklaus@outlook.com>
2018-01-21 19:40:57 +0100

    Let SpatialEntityManager use abstract IDManager interface

---
+++ components/spatial_entity/backing_id_manager.hpp
@@ -0,0 +1,75 @@
+#ifndef BACKING_ID_MANAGER_H
+#define BACKING_ID_MANAGER_H
+
+namespace MFGame
+{
+
+Id *BackingIDManager::getHandle(Id ident, bool warn)
+{
+    if (ident.Index < 0 || ident.Index > mIndices.size())
+    {
+        if (warn)
+            MFLogger::ConsoleLogger::warn("ID is outside of the bounds.", IDMANAGER_MODULE_STR);
+        return nullptr;
+    }
+    else if (mIndices.at(ident.Index) != ident)
+    {
+        if (warn)
+            MFLogger::ConsoleLogger::warn("Invalid ID.", IDMANAGER_MODULE_STR);
+        return nullptr;
+    }
+
+    auto id = &mIndices.at(ident.Index);
+    return id;
+}
+
+uint32_t BackingIDManager::getSlot(Id ident)
+{
+    auto id = getHandle(ident);
+
+    if (id == nullptr)
+    {
+        return NullId.Index;
+    }
+
+    return id->Index;
+}
+
+bool BackingIDManager::isValid(Id ident)
+{
+    auto id = getHandle(ident, false);
+
+    return (id != nullptr);
+}
+
+Id BackingIDManager::allocate()
+{
+    if (mFreeIndices.size() > 0)
+    {
+        auto id = mFreeIndices.back();
+        mFreeIndices.pop_back();
+        return id;
+    }
+
+    Id id(mIndices.size(), 0);
+    mIndices.push_back(id);
+
+    if (id.Index == INT_MAX)
+    {
+        MFLogger::ConsoleLogger::fatal("Maximum number of IDs has been reached!", IDMANAGER_MODULE_STR);
+        return NullId;
+    }
+
+    return id;
+}
+
+void BackingIDManager::deallocate(Id ident)
+{
+    auto id = getHandle(ident);
+    id->Generation++;
+    mFreeIndices.push_back(*id);
+}
+
+}
+
+#endif // BACKING_ID_MANAGER_H
