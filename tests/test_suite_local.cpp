3e715398d74caf768dba3210950d7b81412c8b4a
Miloslav Číž <tastyfish@seznam.com>
2018-01-29 17:05:01 +0100

    Add basic tests

---
+++ tests/test_suite_local.cpp
@@ -0,0 +1,13 @@
+/*
+  These are tests that require the original game data, which cannot be
+  distributed with OpenMF sources.
+*/
+
+#include <tests_general.hpp>
+
+int main()
+{
+    printHeader("LOCAL TESTS");   
+
+    return 0;
+}
