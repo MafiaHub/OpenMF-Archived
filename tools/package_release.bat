280febcd836f6ae05719dc5fb31bf623277b74c0
Dominik Madarász <zaklaus@outlook.com>
2018-04-07 11:15:03 +0200

    Packaging script for Windows

---
+++ tools/package_release.bat
@@ -0,0 +1,31 @@
+@echo off
+
+if not defined OPENMF_INSTALL_DIR (
+	echo OPENMF_INSTALL_DIR has to be defined!
+	exit
+)
+
+echo Packaging release build of OpenMF...
+
+echo Purging previous releases...
+pushd %OPENMF_INSTALL_DIR%
+del * /f/q/S
+popd
+
+echo Copying binaries...
+xcopy /e %OPENMF_DIR%\build\bin\Release\* %OPENMF_INSTALL_DIR%\
+
+echo Copying OpenSceneGraph DLLs...
+xcopy /e %OSG_DIR%\build\bin %OPENMF_INSTALL_DIR%\
+
+echo Copying SDL2.dll ...
+xcopy %SDL2_ROOT%\lib\x64\SDL2.dll %OPENMF_INSTALL_DIR%\
+
+echo Copying zlib.dll ...
+xcopy %OSG_THIRD_PARTY_DIR%\bin\zlib.dll %OPENMF_INSTALL_DIR%\
+
+echo Purging extra files...
+pushd %OPENMF_INSTALL_DIR%
+del *.exp
+del *.lib
+popd
